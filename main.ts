475ab3288dc91cf0b67f64c476cb2f4685329577
Boboss76 <68131019+Boboss76@users.noreply.github.com>
2023-04-09 23:29:12 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+input.calibrateCompass()
+basic.forever(function () {
+    if (input.compassHeading() <= 90 || input.compassHeading() >= 270) {
+        basic.showArrow(ArrowNames.North)
+    } else {
+        basic.showArrow(ArrowNames.South)
+    }
+})
